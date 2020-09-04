c257ffb2ba9291aa7822943dcf8decd65b1d9e6a
Dzianis Shcharbakou <Dzianis_Shcharbakou@epam.com>
2020-09-04 20:24:53 +0300

    custom exception

---
+++ TicketManagement/TicketManagement.BLL/Util/TicketManagementException.cs
@@ -0,0 +1,27 @@
+﻿// ****************************************************************************
+// <copyright file="TicketManagementException.cs" company="EPAM Systems">
+// Copyright (c) EPAM Systems. All rights reserved.
+// Author Dzianis Shcharbakou.
+// </copyright>
+// ****************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketManagement.BLL.Util
+{
+    public class TicketManagementException : Exception
+    {
+        public TicketManagementException(string message, string prop)
+            : base(message)
+        {
+            this.Property = prop;
+            this.Source = prop;
+        }
+
+        public string Property { get; protected set; }
+    }
+}
