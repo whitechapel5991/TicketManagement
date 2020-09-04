7c6fd66a08ccc6bb9bfa8d29e6a6d8d268ebcbb4
Dzianis Shcharbakou <Dzianis_Shcharbakou@epam.com>
2020-09-04 20:24:21 +0300

    bll autofac module

---
+++ TicketManagement/TicketManagement.BLL/Util/BllAutofacModule.cs
@@ -0,0 +1,32 @@
+﻿// ****************************************************************************
+// <copyright file="BllAutofacModule.cs" company="EPAM Systems">
+// Copyright (c) EPAM Systems. All rights reserved.
+// Author Dzianis Shcharbakou.
+// </copyright>
+// ****************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using AutoMapper.Contrib.Autofac.DependencyInjection;
+using TicketManagement.BLL.Interfaces;
+using TicketManagement.BLL.Services;
+using TicketManagement.DAL.Repositories.Base;
+using TicketManagement.DAL.Util;
+
+namespace TicketManagement.BLL.Util
+{
+    public class BllAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<VenueService>()
+                .As<IVenueService>()
+                .InstancePerLifetimeScope();
+            builder.AddAutoMapper(typeof(BllAutofacModule).Assembly);
+        }
+    }
+}
