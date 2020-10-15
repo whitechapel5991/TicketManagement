9bee467c7a350057c14adc6f2a32f1293244e8aa
Dzianis Shcharbakou <Dzianis_Shcharbakou@epam.com>
2020-10-16 01:02:35 +0300

    dataTimeHelper

---
+++ TicketManagement/TicketManagement.BLL/Infrastructure/Helpers/DataTimeHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketManagement.BLL.Infrastructure.Helpers.Interfaces;
+
+namespace TicketManagement.BLL.Infrastructure.Helpers
+{
+    public class DataTimeHelper : IDataTimeHelper
+    {
+        public DateTime GetDateTimeNow()
+        {
+            return DateTime.Now;
+        }
+    }
+}
