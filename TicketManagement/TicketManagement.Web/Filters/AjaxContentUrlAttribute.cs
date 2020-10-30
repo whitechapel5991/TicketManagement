7b5280b7fdb7ce0bab13d96aa5478f11ebc1efdb
Dzianis Shcharbakou <Dzianis_Shcharbakou@epam.com>
2020-10-30 03:08:03 +0300

    filter for contant updating

---
+++ TicketManagement/TicketManagement.Web/Filters/AjaxContentUrlAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TicketManagement.Web.Filters
+{
+    public class AjaxContentUrlAttribute : FilterAttribute, IActionFilter
+    {
+        public static string CurrentContentUrl { get; private set; } = "/TicketManagement.Web/Account/Login";
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            string action = filterContext.ActionDescriptor.ActionName;
+            string controller = filterContext.Controller.GetType().Name.Replace("Controller", string.Empty);
+            CurrentContentUrl = $"/TicketManagement.Web/{controller}/{action}";
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // ignore
+        }
+    }
+}
