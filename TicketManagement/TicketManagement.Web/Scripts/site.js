aae6bc40b45f641b020dccc4a2050628385202e7
Dzianis Shcharbakou <Dzianis_Shcharbakou@epam.com>
2020-10-30 03:03:40 +0300

    add jquery and custom scripts

---
+++ TicketManagement/TicketManagement.Web/Scripts/site.js
@@ -0,0 +1,40 @@
+﻿function reloadPage(response) {
+    if (response.success) {
+        $.ajax({
+                url: '/TicketManagement.Web/',
+                contentType: 'application/html; charset=utf-8',
+                type: 'GET',
+                dataType: 'html',
+                success: function(result) {
+                    reloadContentWithMenu(response.updateContentUrl);
+                }
+            });
+    }
+}
+
+function redirectToMainPage(response) {
+    if (response.success) {
+        window.location = '/TicketManagement.Web/';
+    }
+}
+
+function loadEvents() {
+    reloadContentWithMenu('/TicketManagement.Web/Event/Events');
+}
+
+function loadEventAreaDetails(eventAreaId) {
+    reloadContentWithMenu('/TicketManagement.Web/Event/EventAreaDetail?eventAreaId=' + eventAreaId);
+}
+
+function loadEventManagerEvents() {
+    reloadContentWithMenu('/TicketManagement.Web/EventManager/Event');
+}
+
+function reloadContentWithMenu(contentUrl) {
+    if (contentUrl === '/TicketManagement.Web/StartApp/Index') {
+        document.location.reload();
+    } else {
+        $('#menu').load('/TicketManagement.Web/StartApp/ReloadMenu');
+        $('#content').load(contentUrl);
+    }
+}
