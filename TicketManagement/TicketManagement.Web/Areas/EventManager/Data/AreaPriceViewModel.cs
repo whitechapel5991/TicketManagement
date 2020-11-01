a31a68f31bf3d0f07c9bb91c645ab94dcef95e7e
Dzianis Shcharbakou <Dzianis_Shcharbakou@epam.com>
2020-11-02 01:40:32 +0300

    rename

---
+++ TicketManagement/TicketManagement.Web/Areas/EventManager/Data/AreaPriceViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketManagement.Web.Areas.EventManager.Data
+{
+    public class AreaPriceViewModel
+    {
+        public int EventAreaId { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Range(typeof(decimal), "0", "99999999")]
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        [Display(Name = "Price", ResourceType = typeof(Resources.TicketManagementResource))]
+        public decimal Price { get; set; }
+    }
+}
