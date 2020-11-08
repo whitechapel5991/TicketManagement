66092ada3bf7d1c396aab61d12dfb1666e4b54c8
Dzianis Shcharbakou <Dzianis_Shcharbakou@epam.com>
2020-11-09 01:59:37 +0300

    Add UserProfile exceptions

---
+++ TicketManagement/TicketManagement.Web/Exceptions/UserProfile/ChangePasswordException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketManagement.Web.Exceptions.UserProfile
+{
+    public class ChangePasswordException : Exception
+    {
+        public ChangePasswordException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ TicketManagement/TicketManagement.Web/Exceptions/UserProfile/UpdateUserProfileException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketManagement.Web.Exceptions.UserProfile
+{
+    public class UpdateUserProfileException : Exception
+    {
+        public UpdateUserProfileException(string message)
+            : base(message)
+        {
+        }
+    }
+}
