033932ae5a3c96c3a87ca629936489635918333d
Dzianis Shcharbakou <Dzianis_Shcharbakou@epam.com>
2020-11-16 02:39:23 +0300

    scripts orderer

---
+++ TicketManagement/TicketManagement.Web/App_Start/Extensions/BundleExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Optimization;
+
+namespace TicketManagement.Web.Extensions
+{
+    internal static class BundleExtensions
+    {
+        public static Bundle UnorderedBundling(this Bundle bundle)
+        {
+            bundle.Orderer = new UnorderedBundleOrderer();
+            return bundle;
+        }
+    }
+}
